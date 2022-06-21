b37a07bc4de97fa9ec81e0a153f7410023a92bc1
sector <75115941+onesector@users.noreply.github.com>
2022-06-22 00:19:10 +0300

    update gradle files from groovy to kotlin scripts

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+buildscript {
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.2.2")
+        classpath("com.google.gms:google-services:4.3.10")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.0")
+
+        val hilt = "2.39.1"
+        classpath("com.google.dagger:hilt-android-gradle-plugin:$hilt")
+    }
+}
+
+plugins {
+    id("com.android.application") version "7.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.5.31" apply false
+}
+
+val clean by tasks.creating(Delete::class) {
+    delete = setOf(rootProject.buildDir)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,5 +12,6 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+
 rootProject.name = "UserSystem"
-include ':app'
+include(":app")
