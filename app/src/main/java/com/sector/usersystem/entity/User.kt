10f1eceb424b3ccf501c09dd089208b13fb50f02
sector <75115941+onesector@users.noreply.github.com>
2022-06-28 03:13:29 +0300

    make entity parcelable

--- app/src/main/java/com/sector/usersystem/entity/User.kt
+++ app/src/main/java/com/sector/usersystem/entity/User.kt
@@ -1,13 +1,16 @@
 package com.sector.usersystem.entity
 
+import android.os.Parcelable
 import androidx.annotation.Keep
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
 @Keep
+@Parcelize
 @Entity(tableName = "users")
 data class User(
     @PrimaryKey(autoGenerate = true)
     val id: Int,
     val name: String
-)
+): Parcelable
